e9b0266746d271e6a5d1fcebde6a23edf94d7a5d
Fabricio <fabricioig863@gmail.com>
2021-04-23 15:09:14 -0300

    inicializando projeto Tem-saude

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,11 @@
+function SignUp() {
+  return (
+  <div>
+    <h1>
+      TELA DE CADASTRO
+    </h1>
+  </div>
+  );
+}
+
+export default SignUp;
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyCg-8NaMpWSkpBqVJlsaUYNQ3V2N0LT7yM",
+  authDomain: "tem-web-master.firebaseapp.com",
+  projectId: "tem-web-master",
+  storageBucket: "tem-web-master.appspot.com",
+  messagingSenderId: "864830673353",
+  appId: "1:864830673353:web:80ef266f7acd04b7339818",
+  measurementId: "G-K29ZSLM1G3"
+};
+
+if(!firebase.app.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
